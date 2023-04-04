fc03096ac57fb8cd87bf704159764972da815dc1
mandry24 <herymandrindra@gmail.com>
2023-04-04 11:15:52 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import {
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+
+interface subscriptionPlan {
+  durationMonths: number;
+  priceUsdperGb: number;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  title = 'subscription-order';
+  subscriptionPlans: subscriptionPlan[] = [
+    {
+      durationMonths: 3,
+      priceUsdperGb: 3,
+    },
+    {
+      durationMonths: 6,
+      priceUsdperGb: 2.5,
+    },
+    {
+      durationMonths: 12,
+      priceUsdperGb: 2,
+    },
+  ];
+
+  params = this._formBuilder.group({
+    price: [2, Validators.required],
+    amountGb: [5, Validators.required],
+    upFront: [false, Validators.required]
+  });
+  payment = this._formBuilder.group({
+    cardNumber: ['', Validators.required],
+    expirationDate: ['', Validators.required],
+    cardCode:['', Validators.required]
+  });
+  confirmation = this._formBuilder.group({
+    email: ['', Validators.required],
+    check: [false, Validators.required],
+
+  });
+  isLinear = false;
+  totalPrice : number | null = null;
+  priceperGb: string = '';
+
+  constructor(private _formBuilder: FormBuilder) {
+  }
+
+
+  public validSubscription () {
+    const { price , amountGb, upFront } = this.params.value
+    if(price && amountGb){
+      this.totalPrice = price * amountGb
+      this.priceperGb = `${price}/GB`
+       console.log(upFront)
+      if(upFront)
+        this.totalPrice = this.totalPrice / 10 ;
+    }
+   
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MatInputModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
